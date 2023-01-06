e09d62f1db2250023a2365c38ca1219cfd38797c
nitika02 <garg.nitika1998@gmail.com>
2023-01-07 01:02:52 +0530

    commit

--- dineout/src/All Routes/AllRoutes.jsx
+++ dineout/src/All Routes/AllRoutes.jsx
@@ -4,6 +4,8 @@ import Product from "../Components/ProductPages/Product";
 import Navbar from "../Components/Navbar/Navbar";
 import Checkout from "../Components/Checkout/Checkout";
 import ProductDetails from "../Components/ProductDetail/ProductDetails";
+import GoToTop from "../Components/Home/GoToTop/GoToTop"
+import Blog from "../Components/Blog/Blog";
 
 function AllRoutes() {
   return (
@@ -14,7 +16,9 @@ function AllRoutes() {
         <Route path="/products" element={<Product />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/products/:e" element={<ProductDetails />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
+      <GoToTop />
     </div>
   );
 }
