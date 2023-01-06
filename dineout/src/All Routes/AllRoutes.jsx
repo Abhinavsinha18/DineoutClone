991fa12f9a1a1449a85fbe8490ea5e55ca48f8ec
nitika02 <garg.nitika1998@gmail.com>
2023-01-06 22:08:28 +0530

    added

--- dineout/src/All Routes/AllRoutes.jsx
+++ dineout/src/All Routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import Home from "../Components/Home/home"
 import Product from "../Components/ProductPages/Product"
 import Navbar from "../Components/Navbar/Navbar"
 import Blog from "../Components/Blog/Blog"
+import GoToTop from "../Components/Home/GoToTop/GoToTop"
 
 function AllRoutes() {
     return (
@@ -12,7 +13,8 @@ function AllRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/products" element={<Product />} />
                 <Route path="/blog" element={<Blog />} />
-            </Routes>  
+            </Routes> 
+            <GoToTop /> 
         </div>
     )
 }
