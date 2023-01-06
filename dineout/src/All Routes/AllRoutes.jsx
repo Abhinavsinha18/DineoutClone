39216171c7013f0c6d7fc8688519904716d31935
nitika02 <garg.nitika1998@gmail.com>
2023-01-06 22:03:05 +0530

    added icons

--- dineout/src/All Routes/AllRoutes.jsx
+++ dineout/src/All Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom"
 import Home from "../Components/Home/home"
 import Product from "../Components/ProductPages/Product"
 import Navbar from "../Components/Navbar/Navbar"
+import Blog from "../Components/Blog/Blog"
 
 function AllRoutes() {
     return (
@@ -10,6 +11,7 @@ function AllRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/products" element={<Product />} />
+                <Route path="/blog" element={<Blog />} />
             </Routes>  
         </div>
     )
