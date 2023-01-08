6ee6fe332633b1d6cd5d27b7f1ca1c4ca5e18329
spritualrahul <98818584+spritualrahul@users.noreply.github.com>
2023-01-08 09:06:39 +0530

    navbar login button updated

--- dineout/src/Components/Navbar/LoginButton.jsx
+++ dineout/src/Components/Navbar/LoginButton.jsx
@@ -1,17 +1,12 @@
 
-function LoginButton({setLogin,login}) {
-    const hanldeSubmit = (event) => {
-        console.log('loginHere:',login)
-    };
-    return (
-        <div >
-            <button className="loginButton" onClick={hanldeSubmit}>Login</button>
-        </div>
-    )
-}
-export default LoginButton;
-// // setLogin(true)
-// console.log(setLogin)
-// // localStorage.setItem('loginKey', true);
-// // window.location.reload(true);
-// setLogin(!login);
+// function LoginButton({setLogin,login}) {
+//     const hanldeSubmit = (event) => {
+//         console.log('loginHere:',login)
+//     };
+//     return (
+//         <div >
+//             <button className="loginButton" onClick={hanldeSubmit}>Login</button>
+//         </div>
+//     )
+// }
+// export default LoginButton;
